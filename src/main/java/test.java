a426c5e72d0de329c7b252b988e1eabb3ca3964f
maxim <xerokan@mail.ru>
2021-04-16 18:31:42 +0300

    test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -15,36 +15,47 @@ public class test {
         Map<String, City> cityMap = new HashMap<>();
         Map<String, Miliarder> milMap = new HashMap<>();
         System.out.println(lines.get(0));
-        for (int i = 0; i < Integer.valueOf(lines.get(0).charAt(0)); i++) {
-            int j = 1;
-            while (lines.get(i+1).substring(0, j).lastIndexOf(" ") == -1){
-                j++;
-            }
+        String str = lines.get(1);
+        for(int i = 0; i< Long.valueOf(str); i++){
+            String[] info = lines.get(i+1).split(" ");
 
-            int g = j+1;
-            while (lines.get(i+1).substring(j+1,g).lastIndexOf(" ") == -1){
-                g++;
-            }
-
-            Miliarder mil = new Miliarder(lines.get(i+1).substring(0,j),Long.valueOf(lines.get(i+1).substring(g,lines.get(i+1).length()-1)));
-            City ct = new City(mil,lines.get(i+1).substring(j,g));
-            mil.getCity(ct);
-
+            Miliarder mil = new Miliarder(info[0], Long.valueOf(info[2]));
+            City ct = new City(mil,info[1]);
             cityMap.put(ct.name, ct);
             milMap.put(mil.name, mil);
         }
-        for(Map.Entry<String, City> entry: cityMap.entrySet()) {
-            City value = entry.getValue();
-           System.out.println(value.summ());
-        }
-        for (int i = Integer.valueOf(lines.get(0).charAt(0)); i < lines.size(); i++) {
-            int j = 1;
-            while (lines.get(i+1).substring(0, j).lastIndexOf(" ") == -1){
 
-            }
-        }
+//        for (int i = 0; i < Integer.valueOf(lines.get(0).charAt(0)); i++) {
+//            int j = 1;
+//            while (lines.get(i+1).substring(0, j).lastIndexOf(" ") == -1){
+//                j++;
+//            }
+//
+//            int g = j+1;
+//            while (lines.get(i+1).substring(j+1,g).lastIndexOf(" ") == -1){
+//                g++;
+//            }
+//
+//            Miliarder mil = new Miliarder(lines.get(i+1).substring(0,j),Long.valueOf(lines.get(i+1).substring(g,lines.get(i+1).length()-1)));
+//            City ct = new City(mil,lines.get(i+1).substring(j,g));
+//            mil.getCity(ct);
+//
+//            cityMap.put(ct.name, ct);
+//            milMap.put(mil.name, mil);
+//        }
+//        for(Map.Entry<String, City> entry: cityMap.entrySet()) {
+//            City value = entry.getValue();
+//           System.out.println(value.summ());
+//        }
+//        for (int i = Integer.valueOf(lines.get(0).charAt(0)); i < lines.size(); i++) {
+//            int j = 1;
+//            while (lines.get(i+1).substring(0, j).lastIndexOf(" ") == -1){
+//
+//            }
+//        }
+//    }
+
     }
-
 }
 
 
